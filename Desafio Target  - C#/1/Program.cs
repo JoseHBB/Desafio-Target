443cb55ec76ff1432471100beafff3f25b1ddc76
José Henrique <87625811+JoseHBB@users.noreply.github.com>
2024-09-09 11:44:19 -0300

    Código fonte solicitado
    Feito com o Visual Studio Code

---
+++ Desafio Target  - C#/1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num; // armazena o número a ser recebido
+        bool isOnFibonacci; // armazena se o número pertencera ou não a sequência Fibonacci
+        Console.WriteLine("Insira um número inteiro para ser analisado: ");
+        num = Convert.ToInt32(Console.ReadLine()); // coloca o número digitado pelo usuário
+        isOnFibonacci = Fibonacci(num); // verdadeiro se o número pertence a sequência
+
+        if(isOnFibonacci){ // informa o usuário se o número pertence ou não
+            Console.WriteLine("O número: " + num + " pertence a sequência Fibonacci.");
+        } else {
+            Console.WriteLine("O número: " + num + " não pertence a sequência Fibonacci.");
+        }
+        
+        // numToCheck número ques será feita a checagem
+        // calcula Fibonacci até o número maior ou igual ao número no argumento e retorna verdadeiro caso ele faça parte de fibonnaci e falso caso contrário
+        // também escreve a sequência no console
+        bool Fibonacci(int numToCheck){ 
+            int sum;
+            int a = 0;
+            int b = 1;
+
+            Console.Write("Suquência Fibonacci: ");
+            while(true){
+                sum = a + b;
+                a = b;
+                b = sum;
+                Console.Write(sum);
+                if (numToCheck <= sum){
+                    break;
+                }
+                Console.Write(", ");
+            }
+            Console.WriteLine();
+            if (sum == numToCheck){
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Desafio Target  - C#/2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string? stringToCheck; // declara a variavel string e permite que ela armazene um valor nulo
+        int count = 0; // declara o contador
+        Console.WriteLine("Insira uma string: ");
+        stringToCheck = Console.ReadLine(); // armazena a string na variavel 
+
+        if (stringToCheck == null){ // verifica se a string é nula e encerra caso seja
+            Console.WriteLine("String inválida, por favor tente novamente!");
+            return;
+        } 
+
+        foreach (char word in stringToCheck){ //checa cada char na string e verifica se é A, caso seja adiciona um ao contador
+            if (word == 'a' | word == 'A'){
+                count++;
+            }
+        }
+
+        if(count == 0){ // se o contador for zero informa ao usuário que a palavra não possui a letra A e caso possua informa a ele quantas vezes ela aparece
+            Console.WriteLine("Essa string não possui a letra A minúscula ou maiúscula.");
+        } else {
+            Console.WriteLine("Essa string possuia a letra A e ela apareçe: " + count + " vez(es).");
+        }
+    }
+}
